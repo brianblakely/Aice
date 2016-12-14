695f20c01850adcd31876512b2348c252d5241d4
Brian Blakely <bblakely@contractor.lb.com>
2016-12-14 17:19:09 -0500

    Test style cleanup.

--- renderer.jsx
+++ renderer.jsx
@@ -96,6 +96,7 @@ const layout =
   }}>
     <MuiThemeProvider muiTheme={getMuiTheme(aiceTheme)}>
       <Paper
+        zDepth={0}
         rounded={false}
         style={{
           display: `flex`,
@@ -161,216 +162,214 @@ const layout =
           <ListItem primaryText="Add New Platform" value={3} rightIcon={<IconContentAdd />} />
         </SelectableList>
 
-        <Paper zDepth={2}>
-          <TextField
-            floatingLabelText="Steam Category Label"
-            errorText={window.error && `This field is required`}
-          /><br />
+        <TextField
+          floatingLabelText="Steam Category Label"
+          errorText={window.error && `This field is required`}
+        /><br />
 
-          <TextField
-            floatingLabelText="Nickname"
-          /><br />
+        <TextField
+          floatingLabelText="Nickname"
+        /><br />
 
+        <SelectField
+          floatingLabelText="Emulator"
+          errorText={window.error && `This field is required`}
+        >
+          <MenuItem value={2} primaryText="RetroArch" />
+          <MenuItem value={5} primaryText="Project64" />
+          <MenuItem value={2} primaryText="Dolphin" />
+          <MenuItem value={3} primaryText="ePSXe" />
+          <MenuItem value={1} primaryText="Add New..." />
+        </SelectField><br />
+
+        <Dialog
+          title="Add a New Emulator"
+          actions={emulatorActions}
+          modal={false}
+          open={false}
+          autoScrollBodyContent={true}
+          contentStyle={{
+            maxWidth: `304px`
+          }}
+        >
           <SelectField
             floatingLabelText="Emulator"
             errorText={window.error && `This field is required`}
           >
+            <MenuItem value={1} primaryText="Custom..." />
             <MenuItem value={2} primaryText="RetroArch" />
-            <MenuItem value={5} primaryText="Project64" />
             <MenuItem value={2} primaryText="Dolphin" />
             <MenuItem value={3} primaryText="ePSXe" />
-            <MenuItem value={1} primaryText="Add New..." />
+            <MenuItem value={4} primaryText="Gens" />
+            <MenuItem value={5} primaryText="higan" />
+            <MenuItem value={5} primaryText="Mednafen" />
+            <MenuItem value={5} primaryText="Mupen64plus" />
+            <MenuItem value={5} primaryText="Nestopia" />
+            <MenuItem value={5} primaryText="PCSXR" />
+            <MenuItem value={5} primaryText="PCSX2" />
+            <MenuItem value={5} primaryText="PPSSPP" />
+            <MenuItem value={5} primaryText="Project64" />
+            <MenuItem value={5} primaryText="Snes9x" />
+            <MenuItem value={5} primaryText="VisualBoyAdvance" />
+            <MenuItem value={5} primaryText="Yabause" />
+            <MenuItem value={5} primaryText="ZSNES" />
           </SelectField><br />
 
-          <Dialog
-            title="Add a New Emulator"
-            actions={emulatorActions}
-            modal={false}
-            open={false}
-            autoScrollBodyContent={true}
-            contentStyle={{
-              maxWidth: `304px`
-            }}
-          >
-            <SelectField
-              floatingLabelText="Emulator"
-              errorText={window.error && `This field is required`}
-            >
-              <MenuItem value={1} primaryText="Custom..." />
-              <MenuItem value={2} primaryText="RetroArch" />
-              <MenuItem value={2} primaryText="Dolphin" />
-              <MenuItem value={3} primaryText="ePSXe" />
-              <MenuItem value={4} primaryText="Gens" />
-              <MenuItem value={5} primaryText="higan" />
-              <MenuItem value={5} primaryText="Mednafen" />
-              <MenuItem value={5} primaryText="Mupen64plus" />
-              <MenuItem value={5} primaryText="Nestopia" />
-              <MenuItem value={5} primaryText="PCSXR" />
-              <MenuItem value={5} primaryText="PCSX2" />
-              <MenuItem value={5} primaryText="PPSSPP" />
-              <MenuItem value={5} primaryText="Project64" />
-              <MenuItem value={5} primaryText="Snes9x" />
-              <MenuItem value={5} primaryText="VisualBoyAdvance" />
-              <MenuItem value={5} primaryText="Yabause" />
-              <MenuItem value={5} primaryText="ZSNES" />
-            </SelectField><br />
-
-            <TextField
-              floatingLabelText="Emulator Name"
-              errorText={window.customEmu && `This field is required`}
-              disabled={!window.customEmu}
-            /><br />
-
-            <TextField
-              name="emulator-location"
-              floatingLabelText=""
-              errorText={window.error && `This field is required`}
-              disabled={true}
-            /><br />
-            <FlatButton
-              label="Emulator Location"
-              labelPosition="before"
-              primary={true}
-              icon={<IconFolderOpen />}
-              onClick={()=> {
-                dialog.showOpenDialog(win, {
-                  title: `Emulator Location`,
-                  buttonLabel: `Select`,
-                  properties: [`openFile`, `openDirectory`]
-                }, (e)=> {
-                });
-              }}
-            /><br />
-
-            <TextField
-              name="retroarch-core"
-              floatingLabelText=""
-              errorText={window.retroarch && `This field is required`}
-              disabled={!window.retroarch}
-            /><br />
-            <FlatButton
-              label="RetroArch Core"
-              labelPosition="before"
-              primary={true}
-              icon={<IconFolderOpen />}
-              onClick={()=> {
-                dialog.showOpenDialog(win, {
-                  title: `RetroArch Core`,
-                  buttonLabel: `Select`,
-                  properties: [`openFile`]
-                }, (e)=> {
-                });
-              }}
-              disabled={!window.retroarch}
-            /><br />
-
-            <TextField
-              floatingLabelText="RetroArch Core Name"
-              errorText={window.retroarch && `This field is required`}
-              disabled={!window.retroarch}
-            /><br />
-
-            <TextField
-              floatingLabelText="Command"
-              errorText={window.customEmu && `This field is required`}
-              disabled={!window.customEmu}
-            />
-          </Dialog>
+          <TextField
+            floatingLabelText="Emulator Name"
+            errorText={window.customEmu && `This field is required`}
+            disabled={!window.customEmu}
+          /><br />
 
           <TextField
-            name="rom-folder"
+            name="emulator-location"
             floatingLabelText=""
             errorText={window.error && `This field is required`}
             disabled={true}
           /><br />
           <FlatButton
-            label="ROM Folder"
+            label="Emulator Location"
             labelPosition="before"
             primary={true}
             icon={<IconFolderOpen />}
             onClick={()=> {
               dialog.showOpenDialog(win, {
-                title: `ROM Folder`,
+                title: `Emulator Location`,
                 buttonLabel: `Select`,
-                properties: [`openDirectory`]
+                properties: [`openFile`, `openDirectory`]
               }, (e)=> {
               });
             }}
-          />
-
-          <TextField
-            hintText="File Extensions"
           /><br />
-          <div style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-          }}>
-            <Chip
-              onRequestDelete={()=>{}}
-              style={{
-                margin: `4px`
-              }}
-            >
-              sfc
-            </Chip>
-            <Chip
-              onRequestDelete={()=>{}}
-              style={{
-                margin: `4px`
-              }}
-            >
-              smc
-            </Chip>
-          </div>
 
           <TextField
-            name="fallback-icons-folder"
+            name="retroarch-core"
             floatingLabelText=""
-            disabled={true}
+            errorText={window.retroarch && `This field is required`}
+            disabled={!window.retroarch}
           /><br />
           <FlatButton
-            label="Fallback Icons Folder"
+            label="RetroArch Core"
             labelPosition="before"
             primary={true}
             icon={<IconFolderOpen />}
             onClick={()=> {
               dialog.showOpenDialog(win, {
-                title: `Fallback Icons Folder`,
+                title: `RetroArch Core`,
                 buttonLabel: `Select`,
-                properties: [`openDirectory`]
-              }, (e)=> {
-              });
-            }}
-          />
-
-          <TextField
-            floatingLabelText="Game Title Prefix"
-          /><br />
-
-          <TextField
-            name="default-icon"
-            floatingLabelText=""
-            disabled={true}
-          /><br />
-          <FlatButton
-            label="Default Icon"
-            labelPosition="before"
-            primary={true}
-            icon={<IconFolderOpen />}
-            onClick={()=> {
-              dialog.showOpenDialog(win, {
-                title: `Default Icon`,
-                buttonLabel: `Select`,
-                filters: [{
-                  name: `Steam Grid Icon`,
-                  extensions: [`png`, `jpg`, `tga`]
-                }],
                 properties: [`openFile`]
               }, (e)=> {
               });
             }}
+            disabled={!window.retroarch}
+          /><br />
+
+          <TextField
+            floatingLabelText="RetroArch Core Name"
+            errorText={window.retroarch && `This field is required`}
+            disabled={!window.retroarch}
+          /><br />
+
+          <TextField
+            floatingLabelText="Command"
+            errorText={window.customEmu && `This field is required`}
+            disabled={!window.customEmu}
           />
-        </Paper>
+        </Dialog>
+
+        <TextField
+          name="rom-folder"
+          floatingLabelText=""
+          errorText={window.error && `This field is required`}
+          disabled={true}
+        /><br />
+        <FlatButton
+          label="ROM Folder"
+          labelPosition="before"
+          primary={true}
+          icon={<IconFolderOpen />}
+          onClick={()=> {
+            dialog.showOpenDialog(win, {
+              title: `ROM Folder`,
+              buttonLabel: `Select`,
+              properties: [`openDirectory`]
+            }, (e)=> {
+            });
+          }}
+        />
+
+        <TextField
+          hintText="File Extensions"
+        /><br />
+        <div style={{
+          display: `flex`,
+          flexWrap: `wrap`,
+        }}>
+          <Chip
+            onRequestDelete={()=>{}}
+            style={{
+              margin: `4px`
+            }}
+          >
+            sfc
+          </Chip>
+          <Chip
+            onRequestDelete={()=>{}}
+            style={{
+              margin: `4px`
+            }}
+          >
+            smc
+          </Chip>
+        </div>
+
+        <TextField
+          name="fallback-icons-folder"
+          floatingLabelText=""
+          disabled={true}
+        /><br />
+        <FlatButton
+          label="Fallback Icons Folder"
+          labelPosition="before"
+          primary={true}
+          icon={<IconFolderOpen />}
+          onClick={()=> {
+            dialog.showOpenDialog(win, {
+              title: `Fallback Icons Folder`,
+              buttonLabel: `Select`,
+              properties: [`openDirectory`]
+            }, (e)=> {
+            });
+          }}
+        />
+
+        <TextField
+          floatingLabelText="Game Title Prefix"
+        /><br />
+
+        <TextField
+          name="default-icon"
+          floatingLabelText=""
+          disabled={true}
+        /><br />
+        <FlatButton
+          label="Default Icon"
+          labelPosition="before"
+          primary={true}
+          icon={<IconFolderOpen />}
+          onClick={()=> {
+            dialog.showOpenDialog(win, {
+              title: `Default Icon`,
+              buttonLabel: `Select`,
+              filters: [{
+                name: `Steam Grid Icon`,
+                extensions: [`png`, `jpg`, `tga`]
+              }],
+              properties: [`openFile`]
+            }, (e)=> {
+            });
+          }}
+        />
 
         <IconButton
           style={{
